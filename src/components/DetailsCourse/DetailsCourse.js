f9e7e3152a041f9db27a2f76c875248037e98dfe
iamrealfarhanbd <iamfarhan09@gmail.com>
2022-04-18 22:49:47 +0600

    responsive fix

--- src/components/DetailsCourse/DetailsCourse.js
+++ src/components/DetailsCourse/DetailsCourse.js
@@ -5,13 +5,15 @@ import { Link, useParams } from 'react-router-dom';
 const DetailsCourse = () => {
     const { courseId } = useParams();
     return (
-        <Container className='text-center'>
+        <section class="p-5 bg-dark text-light" id="learn ">
+        <Container className='text-center '>
             <Row>
                 <h2>DetailsCourse Id :{courseId} </h2>
                 <Link to={'/order'}>DetailsCourse </Link>
             </Row>
         </Container>
+        </section>
     );
-};
+};   
 
 export default DetailsCourse;
